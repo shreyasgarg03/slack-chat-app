28947030d5a1ca6b55a1db3ecafc27cec61b3895
Shubham Kashyap <hex.media.1800@gmail.com>
2021-08-18 23:16:48 +0530

    adding a new channel

--- src/components/SidePanel/Channels.jsx
+++ src/components/SidePanel/Channels.jsx
@@ -14,12 +14,46 @@ import {
   Button,
 } from 'semantic-ui-react';
 
+import firebase from '../../utils/firebase';
+
 class Channels extends Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: '',
-    channelDetails: '',
+    channelDetail: '',
     modal: false,
+    channelRef: firebase.database().ref('channles'),
+  };
+
+  isFormValid = ({ channelName, channelDetail }) => {
+    return channelName && channelDetail ? true : false;
+  };
+
+  addChannle = () => {
+    const { channelRef, channelDetail, channelName, user } = this.state;
+
+    const key = channelRef.push().key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetail,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL,
+      },
+    };
+    channelRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: '', channelDetail: '' });
+        this.closeModal();
+        console.log('channel added');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   openModal = () => {
@@ -33,6 +67,13 @@ class Channels extends Component {
     this.setState({
       [e.target.name]: e.target.value,
     });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannle();
+    }
   };
 
   render() {
@@ -51,7 +92,7 @@ class Channels extends Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <ModalHeader>Add a Channel</ModalHeader>
           <ModalContent>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <FormField>
                 <Input
                   fluid
@@ -72,12 +113,12 @@ class Channels extends Component {
           </ModalContent>
           {/* action */}
           <ModalActions>
-            <Button color='green' inverted>
-              <Icon name='checkmark' on /> Add
+            <Button color='green' inverted onClick={this.handleSubmit}>
+              <Icon name='checkmark' /> Add
             </Button>
 
-            <Button color='red' inverted>
-              <Icon name='remove' onClick={this.closeModal} /> Cancel
+            <Button color='red' inverted onClick={this.closeModal}>
+              <Icon name='remove' /> Cancel
             </Button>
           </ModalActions>
         </Modal>
